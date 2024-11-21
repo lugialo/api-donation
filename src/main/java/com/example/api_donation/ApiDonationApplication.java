9875b5cb9000a02e6dd2663aa7c5460c119bb188
Gabriel Antonin Pascoali <gabrielantonin999@gmail.com>
2024-11-21 06:37:01 -0300

    Primeiro commit

---
+++ src/main/java/com/example/api_donation/ApiDonationApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_donation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDonationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDonationApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/api_donation/model/Donation.java
@@ -0,0 +1,16 @@
+package com.example.api_donation.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class Donation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String categoria;
+    private
+
+}
